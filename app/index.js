daddf17bdc2c47334cb520be572a0c7b46b66240
albi-mema <amema@unitsystem.net>
2022-06-13 11:58:11 +0200

    Made changes

--- app/index.js
+++ app/index.js
@@ -1,3 +1,8 @@
-const calc = require('./concat')
+const concat = require('./concat')
+const calc   = require('./calc')
+const fs     = require('fs')
+
+fs.readFile('numbers.json','utf8',(data) => console.log(data))
+
 
 
