ef7728e9b1e933469761fabeedb1b246313122e2
albi-mema <amema@unitsystem.net>
2022-06-13 12:21:27 +0200

    Added reading from file

--- app/index.js
+++ app/index.js
@@ -2,7 +2,12 @@ const concat = require('./concat')
 const calc   = require('./calc')
 const fs     = require('fs')
 
-fs.readFile('numbers.json','utf8',(data) => console.log(data))
+
+const filename = './app/numbers.json'
+
+var content = JSON.parse(fs.readFileSync(filename,'utf8')).numbers
+
+console.log(content)
 
 
 
