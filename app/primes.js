0e9b53225403d24d65ca02cbd4d9d21f789df300
albi-mema <amema@unitsystem.net>
2022-06-13 14:10:33 +0200

    Changed range function to use the one from lodash

--- app/primes.js
+++ app/primes.js
@@ -1,18 +1,8 @@
 const _ = require('lodash')
 
 
+prime = (number) => _.filter(_.range(2,number),(x) => number % x == 0).length == 0
 
-function range(start,end){
-    let numbers = [];
-    for(let i = start;i < end;i++){
-        numbers.push(i)
-    }
-    return numbers
-}
-
-
-prime = (number) => _.filter(range(2,number),(x) => number % x == 0).length == 0
-
-primes_up_to = (limit) => _.filter(range(2,limit),(x) => prime(x))
+primes_up_to = (limit) => _.filter(_.range(2,limit),(x) => prime(x))
 
 module.exports.primes_up_to = primes_up_to
