551bd2d5fe23bec56a3603278ffba4f1e7a3a6b1
albi-mema <amema@unitsystem.net>
2022-06-13 14:49:02 +0200

    added a simple function to test method chaining

--- app/index.js
+++ app/index.js
@@ -1,22 +1,36 @@
-const concat = require('./concat')
-const calc   = require('./calc')
-const fs     = require('fs')
-const prime  = require('./primes')
-const _      = require('lodash')
-let filename = './app/numbers.json'
+const concat     = require("./concat");
+const calc       = require("./calc");
+const fs         = require("fs");
+const prime      = require("./primes");
+const _          = require("lodash");
+const palindrome = require("./palindrome")
 
-let content = JSON.parse(fs.readFileSync(filename,'utf8')).numbers
+let filename   = "./app/numbers.json";
 
-let concated      = concat.concat(content,[16,17,18,19])
-let calculated    = calc.sum(content)
+let content = JSON.parse(fs.readFileSync(filename, "utf8")).numbers;
 
-let limit = 100
-let primNumbers = prime.primes_up_to(limit)
 
-let sumOfPrimes = _.reduce(primNumbers,(x,y) => x + y)
+let concated = concat.concat(content, [16, 17, 18, 19]);
+let calculated = calc.sum(content);
+let word = "hello"
+let reversed  = palindrome.palindrome(word)
 
-console.log(`The rezulting list is ${concated}.`)
-console.log(`The sum is ${calculated}.`)
-console.log(`The prime numbers up to ${limit} are ${primNumbers}.`)
-console.log(`The sum of the first ${limit} prime numbers is ${sumOfPrimes}.`)
 
+let limit = 100;
+let primNumbers = prime.primes_up_to(limit);
+
+let sumOfPrimes = _.reduce(primNumbers, (x, y) => x + y);
+
+function displayIsPalindrome(word){
+    if(reversed){
+        return(`The word ${word} is a palindrome`)
+    }else{
+        return(`The word ${word} is not a palindrome`)
+    }
+}
+
+console.log(`The rezulting list is ${concated}.`);
+console.log(`The sum is ${calculated}.`);
+console.log(`The prime numbers up to ${limit} are ${primNumbers}.`);
+console.log(`The sum of the first ${limit} prime numbers is ${sumOfPrimes}.`);
+console.log(displayIsPalindrome(word))
---
+++ app/palindrome.js
@@ -0,0 +1,14 @@
+const _ = require("lodash");
+
+
+const palindrome = (word) => word ==  
+        _.chain(word)
+        .split("")
+        .reverse()
+        .join("")
+        .value()
+
+
+
+module.exports.palindrome = palindrome
+
