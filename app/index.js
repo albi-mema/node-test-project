823e95ed191598e8ea742c5b1e339657719e04aa
albi-mema <amema@unitsystem.net>
2022-06-13 14:17:53 +0200

    changed vars to lets and created a sum function

--- app/index.js
+++ app/index.js
@@ -2,19 +2,21 @@ const concat = require('./concat')
 const calc   = require('./calc')
 const fs     = require('fs')
 const prime  = require('./primes')
+const _      = require('lodash')
+let filename = './app/numbers.json'
 
-const filename = './app/numbers.json'
-
-var content = JSON.parse(fs.readFileSync(filename,'utf8')).numbers
+let content = JSON.parse(fs.readFileSync(filename,'utf8')).numbers
 
 let concated      = concat.concat(content,[16,17,18,19])
 let calculated    = calc.sum(content)
 
 let limit = 100
-let prime_numbers = prime.primes_up_to(limit)
+let primNumbers = prime.primes_up_to(limit)
 
+let sumOfPrimes = _.reduce(primNumbers,(x,y) => x + y)
 
-console.log(`The rezulting list is ${concated}`)
-console.log(`The sum is ${calculated}`)
-console.log(`The prime numbers up to ${limit} are ${prime_numbers}`)
+console.log(`The rezulting list is ${concated}.`)
+console.log(`The sum is ${calculated}.`)
+console.log(`The prime numbers up to ${limit} are ${primNumbers}.`)
+console.log(`The sum of the first ${limit} prime numbers is ${sumOfPrimes}.`)
 
--- app/primes.js
+++ app/primes.js
@@ -1,8 +1,7 @@
-const _ = require('lodash')
+const _ = require("lodash");
 
+let prime = (number) =>_.filter(_.range(2, number), (x) => number % x == 0).length == 0;
 
-prime = (number) => _.filter(_.range(2,number),(x) => number % x == 0).length == 0
+let primes_up_to = (limit) => _.filter(_.range(2, limit), (x) => prime(x));
 
-primes_up_to = (limit) => _.filter(_.range(2,limit),(x) => prime(x))
-
-module.exports.primes_up_to = primes_up_to
+module.exports.primes_up_to = primes_up_to;
